411240b2d6cb29c2c4eb20ee80941f7c01fbe0d1
EasyEnglishLive <casey_shaun@hotmail.com>
2025-05-30 10:40:20 +0100

    Add files via upload

---
+++ pages/modules/module1/module1_2.jsx
@@ -0,0 +1,3 @@
+export default function Module1_2() {
+  return <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>📘 Module 1, Lesson 2</div>;
+}
---
+++ pages/modules/module2/module2_1.jsx
@@ -0,0 +1,3 @@
+export default function Module2_1() {
+  return <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>📘 Module 2, Lesson 1</div>;
+}
